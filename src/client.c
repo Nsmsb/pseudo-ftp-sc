92ac1d878ac363f859e4411405566db8a4e42e02
Nassim Sbaihia <nassim.sbaihia@gmail.com>
2020-04-05 16:22:25 +0200

    added function to handel responses, print_not found() commands and structred files

--- src/client.c
+++ src/client.c
@@ -3,11 +3,11 @@
 #include "commands.h"
 #include "util.h"
 
-
 int main(int argc, char **argv)
 {
-    int clientfd, port;
-    char *host, buf[MAXLINE];
+    int clientfd, port, cmd_val;
+    char *host, input[MAXLINE], buff[MAXLINE];
+    size_t n;
     rio_t rio;
 
     if (argc != 3) {
@@ -35,15 +35,87 @@ int main(int argc, char **argv)
     
     Rio_readinitb(&rio, clientfd);
 
-    while (Fgets(buf, MAXLINE, stdin) != NULL) {
-        Rio_writen(clientfd, buf, strlen(buf));
-        if (Rio_readlineb(&rio, buf, MAXLINE) > 0) {
-            Fputs(buf, stdout);
-        } else { /* the server has prematurely closed the connection */
-            break;
-        }
+    while (Fgets(input, MAXLINE, stdin) != NULL) {
+
+        // send req
+        printf("input: %s\n", input);
+        send_req(input, strlen(input), clientfd);
+        // Rio_writen(clientfd, input, strlen(input));
+
+        // get res
+        cmd_val = get_res(buff, MAXLINE, rio);
+        printf("response: %d %s\n", cmd_val, buff);
+
+        // handel response
+        handel_res(buff, rio);
+        // get_client("./received", rio);
+        printf("done\n");
+        break;
     }
+
 
     Close(clientfd);
     exit(0);
+}
+
+void print_not_found_err(char *cmd)
+{
+    printf("%s: %scommand not found%s\n", cmd, COL_RED,COL_RST);
+}
+
+void send_req(char *buff, size_t n, int fd)
+{
+    Rio_writen(fd, buff, n);
+}
+
+int get_res(char *buff, size_t n, rio_t fds) {
+    Rio_readlineb(&fds, buff, n);
+
+    return is_valid_cmd(buff);
+}
+
+void handel_res(char *res, rio_t fds) {
+    int cmd_code, args;
+    char **res_data;
+
+
+    cmd_code = is_valid_cmd(res);
+    args = str_split(res, " ", &res_data);
+
+    printf("res data[0] = %s, cmod %d\n", res, cmd_code);
+
+    switch (cmd_code)
+    {
+    case GETV:
+        get_client(res, fds);
+        break;
+    
+    case LSV:
+        printf("commande LS\n");
+        break;
+
+    case PWDV:
+        printf("commande LS\n");
+        break;
+
+    case CDV:
+        printf("commande LS\n");
+        break;
+
+    case RMV:
+        printf("commande LS\n");
+        break;
+
+    case PUTV:
+        printf("commande LS\n");
+        break;
+
+    case EXTV:
+        printf("commande LS\n");
+        break;
+
+    default:
+        print_not_found_err(res_data[0]);
+        break;
+    }
 }
--- src/client.h
+++ src/client.h
@@ -1,6 +1,17 @@
 #ifndef __CLIENT_H__
 #define __CLIENT_H__
 
+#include "csapp.h"
+
+
+void send_req(char *buff, size_t n, int fd);
+
+int get_res(char *buff, size_t n, rio_t fds);
+
+void print_not_found_err(char *cmd);
+
+void handel_res(char *res, rio_t fds);
+
 
 
 
